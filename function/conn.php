ce6f2e0d9331ec75fa2e32fa94acd35a9df98432
antonio99686 <antonio.2022324018@aluno.iffar.edu.br>
2024-07-20 12:10:22 -0300

    email

--- function/conn.php
+++ function/conn.php
@@ -7,3 +7,5 @@ $conn = [
     'email' => 'notificacaosistema3@gmail.com',
     'senha_email' => 'sujfztgschvtwsus'
 ];
+// E-mail: notificacaosistema3@gmail.com
+// Senha: m1ch@3l1s ou M1ch@3l1s ou m1ch@3l1S
